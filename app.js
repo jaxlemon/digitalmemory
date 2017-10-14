f724385f71d4a6b31409b7ac78b8e52abb81deaf
Aixiao Jiang <aijiang@microsoft.com>
2017-10-14 18:26:08 +0800

    1

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
 
+
+app.enable("jsonp callback");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +26,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', index);
 app.use('/users', users);
